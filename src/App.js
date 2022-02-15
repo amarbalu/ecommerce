4ff8eeea34a4ae29057ed2b2314540b10378e917
Amar Balu <inboxamarbalu@gmail.com>
2022-02-15 21:59:22 +0530

    router V6 implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import { routes } from "./routes";
+import PrivateRoute from "./components/PrivateRoute";
 
-function App() {
-  return <div className="App"></div>;
-}
+const history = createBrowserHistory();
+const App = () => {
+  return (
+    <div className="App">
+      <React.Fragment>
+        <BrowserRouter>
+          <Routes>
+            {routes.map((props) =>
+              props.path === "/cartpage" ? (
+                <Route
+                  path={props.path}
+                  element={<PrivateRoute>{props.component}</PrivateRoute>}
+                />
+              ) : (
+                <Route path={props.path} element={props.component} />
+              )
+            )}
+            <Route path="*" element={<Navigate to="/homepage" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </React.Fragment>
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import createSagaMiddleware from "redux-saga";
+import { reducer, initialState } from "./reducer";
+import rootSaga from "./saga";
+import reportWebVitals from "./reportWebVitals";
 
+const saga = createSagaMiddleware();
+const store = createStore(reducer, initialState, applyMiddleware(saga));
+saga.run(rootSaga);
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
